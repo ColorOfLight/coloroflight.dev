fcdb94ef3ed707648ef49e9b289d25480be71c61
Seongho <bluetosh7@gmail.com>
2024-05-03 02:23:28 +0900

    refactor: make shaders into separate files

--- demos/src/app/first-webgpu-app/hooks/useWebGPU.ts
+++ demos/src/app/first-webgpu-app/hooks/useWebGPU.ts
@@ -1,5 +1,9 @@
 import { useRef, useEffect } from "react";
 
+import vertexShader from "../shaders/vertex.wgsl";
+import fragmentShader from "../shaders/fragment.wgsl";
+import computeShader from "../shaders/compute.wgsl";
+
 const GRID_SIZE = 32;
 const UPDATE_INTERVAL = 200;
 const WORKGROUP_SIZE = 8;
@@ -97,93 +101,19 @@ const runWebGPU = async (canvas: HTMLCanvasElement) => {
   }
   device.queue.writeBuffer(cellStateStorage[1], 0, cellStateArray);
 
-  const cellShaderModule = device.createShaderModule({
-    label: "Cell shader",
-    code: `
-      struct VertexInput {
-        @location(0) pos: vec2f,
-        @builtin(instance_index) instance: u32,
-      };
+  const vertexShaderModule = device.createShaderModule({
+    label: "Cell vertex shader",
+    code: vertexShader,
+  });
 
-      struct VertexOutput {
-        @builtin(position) pos: vec4f,
-        @location(0) cell: vec2f,
-      };
-
-      struct FragInput {
-        @location(0) cell: vec2f,
-      };
-
-      @group(0) @binding(0) var<uniform> grid: vec2f;
-      @group(0) @binding(1) var<storage> cellState: array<u32>;
-
-      @vertex
-      fn vertexMain(input: VertexInput) -> VertexOutput  {
-        let i = f32(input.instance);
-        let cell = vec2f(i % grid.x, floor(i / grid.x));
-        let state = f32(cellState[input.instance]);
-
-        let cellOffset = cell / grid * 2;
-        let gridPos = (input.pos * state + 1) / grid - 1 + cellOffset;
-        
-        var output: VertexOutput;
-        output.pos = vec4f(gridPos, 0, 1);
-        output.cell = cell;
-        return output;
-      }
-
-      @fragment
-      fn fragmentMain(input: FragInput) -> @location(0) vec4f {
-        let c = input.cell / grid;
-        return vec4f(c, 1-c.x, 1);
-      }
-  `,
+  const fragmentShaderModule = device.createShaderModule({
+    label: "Cell fragment shader",
+    code: fragmentShader,
   });
 
   const simulationShaderModule = device.createShaderModule({
     label: "Game of Life simulation shader",
-    code: `
-      @group(0) @binding(0) var<uniform> grid: vec2f;
-
-      @group(0) @binding(1) var<storage> cellStateIn: array<u32>;
-      @group(0) @binding(2) var<storage, read_write> cellStateOut: array<u32>;
-
-      fn cellIndex(cell: vec2u) -> u32 {
-        return (cell.y % u32(grid.y)) * u32(grid.x) +
-              (cell.x % u32(grid.x));
-      }
-
-      fn cellActive(x: u32, y: u32) -> u32 {
-        return cellStateIn[cellIndex(vec2(x, y))];
-      }
-
-      @compute
-      @workgroup_size(${WORKGROUP_SIZE}, ${WORKGROUP_SIZE}) // New line
-      fn computeMain(@builtin(global_invocation_id) cell: vec3u) {
-        let activeNeighbors = cellActive(cell.x+1, cell.y+1) +
-                        cellActive(cell.x+1, cell.y) +
-                        cellActive(cell.x+1, cell.y-1) +
-                        cellActive(cell.x, cell.y-1) +
-                        cellActive(cell.x-1, cell.y-1) +
-                        cellActive(cell.x-1, cell.y) +
-                        cellActive(cell.x-1, cell.y+1) +
-                        cellActive(cell.x, cell.y+1);
-        
-        let i = cellIndex(cell.xy);
-
-        // Conway's game of life rules:
-        switch activeNeighbors {
-          case 2: { // Active cells with 2 neighbors stay active.
-            cellStateOut[i] = cellStateIn[i];
-          }
-          case 3: { // Cells with 3 neighbors become or stay active.
-            cellStateOut[i] = 1;
-          }
-          default: { // Cells with < 2 or > 3 neighbors become inactive.
-            cellStateOut[i] = 0;
-          }
-        }
-      }`,
+    code: computeShader,
   });
 
   // Create the bind group layout and pipeline layout.
@@ -260,12 +190,12 @@ const runWebGPU = async (canvas: HTMLCanvasElement) => {
     label: "Cell pipeline",
     layout: pipelineLayout,
     vertex: {
-      module: cellShaderModule,
+      module: vertexShaderModule,
       entryPoint: "vertexMain",
       buffers: [vertexBufferLayout],
     },
     fragment: {
-      module: cellShaderModule,
+      module: fragmentShaderModule,
       entryPoint: "fragmentMain",
       targets: [
         {
