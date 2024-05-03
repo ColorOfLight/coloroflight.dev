8942cf6479c2a78d6b99cef5a0396c1afdd1b13f
Seongho <bluetosh7@gmail.com>
2024-05-03 17:38:17 +0900

    feat: add handles to control game parameters

--- demos/src/app/first-webgpu-app/hooks/useGame.ts
+++ demos/src/app/first-webgpu-app/hooks/useGame.ts
@@ -1,21 +1,54 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useCallback } from "react";
+import { button, useControls } from "leva";
 
 import runGame from "../runner";
 
-const GRID_SIZE = 32;
-const UPDATE_INTERVAL = 200;
-
 const useGame = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null!);
+  const gameDestroyRef = useRef<() => void>();
 
-  // To prevent running a hook twice in development
-  const runningOnceRef = useRef<boolean>(false);
+  // For preventing running twice in development env.
+  const onceRef = useRef<boolean>(false);
+
+  const rerunGame = useCallback(
+    async (gridSize: number, timeInterval: number) => {
+      if (gameDestroyRef.current != null) {
+        gameDestroyRef.current();
+      }
+
+      const destroyNewGame = await runGame(
+        canvasRef.current,
+        gridSize,
+        timeInterval
+      );
+
+      gameDestroyRef.current = destroyNewGame;
+    },
+    []
+  );
+
+  const { gridSize, timeInterval } = useControls({
+    gridSize: { value: 32, min: 8, max: 256, step: 1 },
+    timeInterval: { value: 200, min: (1 / 60) * 1000, max: 1000, step: 1 },
+    rerunGame: button((get) => {
+      rerunGame(get("gridSize"), get("timeInterval"));
+    }),
+  });
 
   useEffect(() => {
-    if (!runningOnceRef.current) {
-      runGame(canvasRef.current, GRID_SIZE, UPDATE_INTERVAL);
-      runningOnceRef.current = true;
-    }
+    (async () => {
+      if (!onceRef.current) {
+        onceRef.current = true;
+
+        const destroyGame = await runGame(
+          canvasRef.current,
+          gridSize,
+          timeInterval
+        );
+        gameDestroyRef.current = destroyGame;
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return { canvasRef };
