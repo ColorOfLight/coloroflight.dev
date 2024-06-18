bedae7fd9ccb30acf1e14e69306967298af8ee18
Seongho <bluetosh7@gmail.com>
2024-06-18 17:07:33 +0900

    home(feat): add personal logo and use it for favicon

--- home/theme.config.jsx
+++ home/theme.config.jsx
@@ -2,13 +2,16 @@ const YEAR = new Date().getFullYear();
 
 const meta = {
   title: "ColorOfLight",
-  description: "ColorOfLight(Seongho Park)'s personal page.",
-  image: "",
+  description: "ColorOfLight's personal page.",
+  image: "/logo.svg",
 };
 
 const head = () => (
   <>
     <title>{meta.title}</title>
+
+    <link rel="icon" href={meta.image} type="image/svg+xml" />
+
     <meta name="robots" content="follow, index" />
     <meta name="description" content={meta.description} />
     <meta property="og:site_name" content={meta.title} />
@@ -16,15 +19,22 @@ const head = () => (
     <meta property="og:title" content={meta.title} />
     <meta property="og:image" content={meta.image} />
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:site" content="@yourname" />
+    <meta name="twitter:site" content="@ColorOf_Light" />
     <meta name="twitter:title" content={meta.title} />
     <meta name="twitter:description" content={meta.description} />
     <meta name="twitter:image" content={meta.image} />
   </>
 );
 
+const footer = (
+  <footer>
+    <p>Powered by Nextra</p>
+    <p className="text-right">{YEAR} © ColorOfLight (Seongho Park)</p>
+  </footer>
+);
+
 export default {
-  footer: <p>{YEAR} © ColorOfLight</p>,
+  footer,
   head,
   darkMode: true,
 };
