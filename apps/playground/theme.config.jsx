43be7293593a17827161d632bc9af403fcfae229
Seongho <bluetosh7@gmail.com>
2024-06-21 15:19:35 +0900

    playground(feat): add logo and fix link

--- apps/playground/theme.config.jsx
+++ apps/playground/theme.config.jsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+
+import LogoImage from "@repo/common-assets/images/logo.svg";
+
+const Logo = () => (
+  <div className="flex gap-4 items-center">
+    <Image src={LogoImage.src} width={32} height={32} alt="ColorOfLight Logo" />
+    <h1 className="font-bold">Playground</h1>
+  </div>
+);
+
 export default {
-  logo: <span>My Nextra Documentation</span>,
+  logo: <Logo />,
   project: {
-    link: 'https://github.com/shuding/nextra'
+    link: 'https://github.com/ColorOfLight/coloroflight.dev/tree/main/apps/playground'
   }
   // ... other theme options
 }
