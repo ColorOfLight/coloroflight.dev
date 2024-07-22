e7c003bce6311b58ef7d75e1865122efb0bd94aa
Seongho <bluetosh7@gmail.com>
2024-07-22 15:21:08 +0900

    playground(feat): update module with flipImage

--- cpps/README.md
+++ cpps/README.md
@@ -18,20 +18,10 @@ cd emsdk
 source ./emsdk_env.sh
 ```
 
-### VCpkg
-
-- Download & Install
-
-```zsh
-git clone https://github.com/microsoft/vcpkg.git
-cd vcpkg
-./bootstrap-vcpkg.sh
-```
-
 ## How to build
 
 ```zsh
 mkdir build && cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=../emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake ..
+cmake --preset=default ..
 make
 ````
---
+++ cpps/src/em_main.cpp
@@ -0,0 +1,37 @@
+#include <emscripten/bind.h>
+
+#include <map>
+#include <random>
+
+#include "./file.h"
+#include "./image.h"
+
+void FlipImage() {
+  Image<RgbaColor> image = ReadPng("/working/input.png");
+  image.FlipY();
+
+  WritePng("/working/output.png", image);
+}
+
+std::map<int, int> simulate_dice_trials(int trials) {
+  std::map<int, int> result;
+
+  // Random number generation using C++ random library
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(1, 6);
+
+  for (int t = 0; t < trials; ++t) {
+    result[dis(gen)]++;
+  }
+
+  return result;
+}
+
+// Binding code
+EMSCRIPTEN_BINDINGS(module) {
+  emscripten::function("flipImage", &FlipImage);
+  emscripten::function("simulate_dice_trials", &simulate_dice_trials);
+  emscripten::register_map<int, int>("map<int, int>");
+  emscripten::register_vector<int>("vector<int>");
+}
